c995664c4fc35966125a4d942d7a5bbbaec55d0d
mthang <m.thang@qfab.org>
2024-03-28 11:18:54 +1000

    Update RemoveDuplicates.sh

--- scripts/RemoveDuplicates.sh
+++ scripts/RemoveDuplicates.sh
@@ -3,7 +3,7 @@
 #Script to remove duplicated sequences by name
 # Needs Seqkit installed
 
-#Change the input filename here
+#Change the input filename here (can be found under "FilteredBySize")
 input=Trechisporales.csv_editfiltered.fasta
 #Change this for output duplicate list
 duplicates=Trechisporales_duplicate_list
